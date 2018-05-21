a95488d300dc4ce2f49a46bd0a48bb2beafa68ae
Gerard Vivancos <gerard.vivancos@gmail.com>
2018-05-22 00:42:26 +0200

    mangling with gyros

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -20,33 +20,40 @@ public class Rocket : MonoBehaviour {
     enum State { Alive, Dying, Transcending };
     State currentState = State.Alive;
     Boolean collisionsDisabled = false;
+    float[] rotationValues = new float[15];
+    float averageRotation = 0;
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
+        if (SystemInfo.supportsGyroscope)
+        {
+            Input.gyro.enabled = true;
+        }
+
     }
 	
 	// Update is called once per frame
 	void Update () {
+        UpdateGyroBaseRotation();
         if (currentState == State.Alive) {
             ProcessInput();
         }
+
 	}
 
     private void ProcessInput() {
         if (SystemInfo.supportsGyroscope) {
             RotateFromGyro();
-            if (Input.GetTouch(0).phase == TouchPhase.Began) {
+            if (Input.GetTouch(0).pressure > 0) {
                 ThrustRocket();
             }
-            if (Input.GetTouch(0).phase == TouchPhase.Ended) {
-                StopRocketThrust();
+            if (Input.GetTouch(0).phase == TouchPhase.Began) {
+                printDebug();
             }
             return;
         }
-
-
 
         if (Input.GetKey(KeyCode.Space)) {
             ThrustRocket();
@@ -144,17 +151,37 @@ public class Rocket : MonoBehaviour {
         rb.AddRelativeForce(0, 0, UnityEngine.Random.Range(-10f, 10f), ForceMode.Impulse);
     }
 
-    private void ReloadCurrentScene()
-    {
+    private void ReloadCurrentScene() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
-    private void LoadNextScene()
-    {
+    private void LoadNextScene() {
         SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCountInBuildSettings);
     }
 
     private void RotateFromGyro() {
-        Rotate(Input.gyro.rotationRateUnbiased.z);
+        //print("Orientation: " + Screen.orientation);
+        float a = averageRotation - Input.gyro.attitude.eulerAngles.z;
+        if (a < -1 || a > 1) {
+            Rotate(a);
+        }
+    }
+
+    private void printDebug(){
+        print("Attitude: " + Input.gyro.attitude.eulerAngles);
+    }
+
+    private void UpdateGyroBaseRotation() {
+        
+        for (int i = 0; i < rotationValues.Length -1 ; i ++) {
+            rotationValues[i] = rotationValues[i + 1];
+        }
+        float a = 0;
+        rotationValues[rotationValues.Length - 1] = Input.gyro.attitude.eulerAngles.z;
+        for (int i = 0; i < rotationValues.Length; i++) {
+            a += rotationValues[i];
+        }
+        averageRotation = a / rotationValues.Length;
+        print(averageRotation - Input.gyro.attitude.eulerAngles.z);
     }
 }
